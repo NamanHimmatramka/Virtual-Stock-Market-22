630a6f60abbc0446c0151e7f183d5a88b8a86660
Naman Himmatramka <himmatramka2003@gmail.com>
2021-11-13 14:53:28 +0530

    fragments_created

--- app/src/main/java/com/example/vsm22/MainActivity.java
+++ app/src/main/java/com/example/vsm22/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomappbar.BottomAppBar;
 
+public class MainActivity extends AppCompatActivity {
+    BottomAppBar bottomAppBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomAppBar = findViewById(R.id.app_bar);
+
     }
 }
