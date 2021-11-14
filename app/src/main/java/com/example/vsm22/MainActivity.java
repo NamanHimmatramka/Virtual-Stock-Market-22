1278f833395516865fdb24ba957f4f96df86b5cc
Naman Himmatramka <himmatramka2003@gmail.com>
2021-11-14 21:31:44 +0530

    Bottom Navigation View added and clicks settled

--- app/src/main/java/com/example/vsm22/MainActivity.java
+++ app/src/main/java/com/example/vsm22/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.vsm22;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomAppBar bottomAppBar;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomAppBar = findViewById(R.id.app_bar);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
 
+        getSupportFragmentManager().beginTransaction().replace(R.id.FL_main, new PortfolioFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.portfolio: getSupportFragmentManager().beginTransaction().replace(R.id.FL_main, new PortfolioFragment()).commit();
+                    return true;
+
+                    case R.id.news: getSupportFragmentManager().beginTransaction().replace(R.id.FL_main, new NewsFragment()).commit();
+                    return true;
+
+                    case R.id.powercards: getSupportFragmentManager().beginTransaction().replace(R.id.FL_main, new PowercardsFragment()).commit();
+                    return true;
+
+                    case R.id.leaderboard: getSupportFragmentManager().beginTransaction().replace(R.id.FL_main, new LeaderboardFragment()).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vsm22/SelectorActivity.java
+++ app/src/main/java/com/example/vsm22/SelectorActivity.java
@@ -1,14 +1,30 @@
 package com.example.vsm22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelectorActivity extends AppCompatActivity {
+
+    CardView mainGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selector);
+
+        mainGame = findViewById(R.id.CV_mainGame);
+
+        mainGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectorActivity.this , MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
