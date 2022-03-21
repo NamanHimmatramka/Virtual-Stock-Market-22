4750aaf9314e24d106edce0b1a17653ffcece48f
Naman Himmatramka <himmatramka2003@gmail.com>
2022-03-21 15:23:34 +0530

    NetWorth Updated

--- app/src/main/java/com/example/vsm22/WaitingActivity2.java
+++ app/src/main/java/com/example/vsm22/WaitingActivity2.java
@@ -9,13 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.vsm22.models.Currency;
 import com.example.vsm22.models.Important;
+import com.example.vsm22.models.Stock;
 import com.example.vsm22.models.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WaitingActivity2 extends AppCompatActivity {
 private Button leaderBoard;
@@ -53,6 +59,61 @@ private FirebaseFirestore db;
                         if(documentSnapshot.exists()){
                             Important important=documentSnapshot.toObject(Important.class);
                             if(important.leaderboard==1){
+                                db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        if(documentSnapshot.exists()){
+                                            User user = documentSnapshot.toObject(User.class);
+                                            user.status = "waiting";
+                                            db.collection("stocks").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                                    List<DocumentSnapshot> ds = queryDocumentSnapshots.getDocuments();
+                                                    ArrayList<Double> stockPrices= new ArrayList<>();
+                                                    for(DocumentSnapshot dsi: ds){
+                                                        stockPrices.add(dsi.toObject(Stock.class).getStockPriceInRupees());
+                                                    }
+                                                    db.collection("crypto").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                                        @Override
+                                                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                                            List<DocumentSnapshot> ds = queryDocumentSnapshots.getDocuments();
+                                                            ArrayList<Double> cryptoPrices= new ArrayList<>();
+                                                            for(DocumentSnapshot dsi: ds){
+                                                                cryptoPrices.add(dsi.toObject(Currency.class).getcryptoPriceInRupees());
+                                                            }
+                                                            double netStockWorth = 0;
+                                                            for(int i=0; i<user.noOfStocksOwned.size(); i++){
+                                                                netStockWorth+=user.noOfStocksOwned.get(i)*stockPrices.get(i);
+                                                            }
+                                                            double netCryptoWorth = 0;
+                                                            for(int i=0; i<user.currencyOwned.size(); i++){
+                                                                netCryptoWorth+=user.currencyOwned.get(i)*cryptoPrices.get(i);
+                                                            }
+                                                            user.netWorth = netStockWorth+netCryptoWorth;
+                                                            db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(user);
+                                                            finish();
+                                                        }
+                                                    }).addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+
+                                                        }
+                                                    });
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+
+                                                }
+                                            });
+                                        }
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                    }
+                                });
                                 startActivity(new Intent(getApplicationContext(),WaitingActivity.class));
                             }
                             else{
