710385e2cf91d05eb5baedc7874516e54bbbc8de
Naman Himmatramka <himmatramka2003@gmail.com>
2022-03-24 19:18:36 +0530

    Nils Slider added

--- app/src/main/java/com/oculus/vsm/SponsorsActivity.java
+++ app/src/main/java/com/oculus/vsm/SponsorsActivity.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SponsorsActivity extends AppCompatActivity {
-private ViewPager2 viewPager2;
+private ViewPager2 viewPager2, viewPager22;
 private Handler slideHandler=new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,8 @@ private Handler slideHandler=new Handler();
         setContentView(R.layout.activity_sponsors);
 
         viewPager2=findViewById(R.id.viewPagerImageSlider);
+        viewPager22=findViewById(R.id.viewPagerImageSlider2);
+        List<SliderItem> sliderItems2 = new ArrayList<>();
         List<SliderItem> sliderItems=new ArrayList<>();
         sliderItems.add(new SliderItem(R.drawable.imag_sp16));
         sliderItems.add(new SliderItem(R.drawable.imag_sp15));
@@ -31,13 +33,24 @@ private Handler slideHandler=new Handler();
         sliderItems.add(new SliderItem(R.drawable.imag_sp12));
         sliderItems.add(new SliderItem(R.drawable.imag_sp11));
 
-
+        sliderItems2.add(new SliderItem(R.drawable.image_spnils1));
+        sliderItems2.add(new SliderItem(R.drawable.image_spnils2));
+        sliderItems2.add(new SliderItem(R.drawable.image_spnils3));
+        sliderItems2.add(new SliderItem(R.drawable.image_spnils4));
+        sliderItems2.add(new SliderItem(R.drawable.image_spnils5));
+        sliderItems2.add(new SliderItem(R.drawable.image_spnils6));
 
         viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
         viewPager2.setClipToPadding(false);
         viewPager2.setClipChildren(false);
         viewPager2.setOffscreenPageLimit(3);
         viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        viewPager22.setAdapter(new SliderAdapter(sliderItems2,viewPager22));
+        viewPager22.setClipToPadding(false);
+        viewPager22.setClipChildren(false);
+        viewPager22.setOffscreenPageLimit(3);
+        viewPager22.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
 
 
         CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
@@ -52,6 +65,7 @@ private Handler slideHandler=new Handler();
         });
 
         viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager22.setPageTransformer(compositePageTransformer);
     }
 
 }
